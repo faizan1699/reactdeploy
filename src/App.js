b408a2c45184db1dedb95ceee43f0c6dccb5df34
faizan1699 <123071794+faizan1699@users.noreply.github.com>
2023-10-24 23:23:26 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
      
       <Routes>
 
-        <Route path='/' element={<Home />} />
+        <Route path='/Home' element={<Home />} />
         <Route path='About' element={<About />} />
         <Route path='Contact' element={<Contact />} />
         <Route path='Services' element={<Services />} />
